37b2169d51c7c02d114c4f93ee2911ebd857e2bc
ravipnsit <prakashravip1@gmail.com>
2022-06-05 19:38:58 +0530

    spark sql delta lake crud library support

--- src/delta_lake/delta_lake_spark.py
+++ src/delta_lake/delta_lake_spark.py
@@ -5,58 +5,62 @@ from src.utils.cores import get_max_cores
 from src.delta_lake.spark_session import configure_spark_session
 from typing import List, Tuple
 
-spark = configure_spark_session(get_max_cores())
+spark = configure_spark_session(cores=get_max_cores(), app_name="delta_client")
 
-class DeltaLakeSpark:
-    def __init__(self, spark_session: SparkSession = None):
-        """
-        using spark sql
-        """
-        if spark_session is None:
-            self.spark = spark
-        else:
-            self.spark = spark_session
+def create_database(database_name: str):
+    create_db_query = f"CREATE DATABASE IF NOT EXISTS {database_name}"
+    try:
+        spark.sql(create_db_query)
+        logger.info(f"{database_name} is created")
+    except Exception as e:
+        logger.exception(f"Exception: {str(e)}")
 
+def create_table_with_schema(
+     db, table: str,
+     schema: List[Tuple], \
+     partition_cols: List[str]):
 
-    def create_database(database_name: str):
-        create_db_query = f"CREATE DATABASE {database_name}"
-        try:
-            self.spark.sql(create_db_query)
-            logger.info(f"{database_name} is created")
-        except Exception as e:
-            logger.exception(f"Exception: {str(e)}")
+     ddl_sql = f"CREATE TABLE {db}.{table} ("
 
-    def create_table_with_schema(
-        self,\
-         db, table: str,
-         schema: List[Tuple], \
-         partition_cols: List[str]):
+     i = 0
+     for col_name, col_type in schema:
+         if i == len(schema)-1:
+             ddl_sql += f"{col_name} {col_type}"
+         else:
+             ddl_sql += f"{col_name} {col_type},"
+         i += 1
 
-         ddl_sql = f"CREATE TABLE {db}.{table} ("
+     ddl_sql += ") USING DELTA "
+     if len(partition_cols) > 0:
+         ddl_sql += f"PARTITIONED BY ({','.join(partition_cols)});"
+     try:
+         spark.sql(ddl_sql)
+     except Exception as e:
+         logger.exception(f"Exception: {str(e)}")
 
-         for i, col_nanme, col_type in enumerate(schema):
-             if i == len(schema)-1:
-                 ddl_sql += f"{col_nanme} {col_type}"
-             else:
-                 ddl_sql += f"{col_nanme} {col_type},"
+def insert(table: str, values: List[Tuple]):
+     insert_query = f"INSERT INTO {table} values ("
+     for value in values:
+         insert_query += "(" + ",".join(value) + ")"
+     insert_query += ");"
+     try:
+         spark.sql(insert_query)
+     except Exception as e:
+         logger.exception(f"Exception: {str(e)}")
 
-         ddl_sql += ") USING DELTA "
-         if len(partition_cols) > 0:
-             ddl_sql += f"[PARTITIONED BY ({','.join(partition_cols)})]"
-         try:
-             self.spark.sql(ddl_sql)
-         except Exception as e:
-             logger.exception(f"Exception: {str(e)}")
+def read():
+    pass
 
-    def insert(self, table: str, values: List[Tuple]):
-         insert_query = f"INSERT INTO {table} values ("
-         for value in values:
-             insert_query += "(" + ",".join(value) + ")"
-         insert_query += ")"
-         try:
-             self.spark.sql(insert_query)
-         except Exception as e:
-             logger.exception(f"Exception: {str(e)}")
+def delete_database(database_name: str):
+    drop_db_query = f"DROP DATABASE IF EXISTS {database_name}"
+    try:
+        spark.sql(drop_db_query)
+        logger.info(f"{database_name} is created")
+    except Exception as e:
+        logger.exception(f"Exception: {str(e)}")
 
-    def update(self):
-        pass
+if __name__ == '__main__':
+    #delete_database("food_db")
+    create_database("food_db")
+    create_table_with_schema("food_db", "indian_food",
+       [("food_type", "STRING"), ("name", "STRING"), ("price", "FLOAT")], ["food_type"])
--- src/utils/cores.py
+++ src/utils/cores.py
@@ -1,4 +1,4 @@
 import os
 
 def get_max_cores() -> int:
-    return os.cpu_cores()
+    return os.cpu_count()
